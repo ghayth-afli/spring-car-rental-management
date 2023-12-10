584280c773f207e1f3f49ec031a0276ce3dfe160
Afli Mohamed Ghayth <40703798+ghayth-afli@users.noreply.github.com>
2023-12-10 04:25:44 +0100

    Update VoitureController.java

--- src/main/java/com/miniprojet/location_des_voitures/controller/VoitureController.java
+++ src/main/java/com/miniprojet/location_des_voitures/controller/VoitureController.java
@@ -1,6 +1,8 @@
 package com.miniprojet.location_des_voitures.controller;
 
 import com.miniprojet.location_des_voitures.dto.requests.VoitureRequest;
+import com.miniprojet.location_des_voitures.model.EStatut;
+import com.miniprojet.location_des_voitures.model.Voiture;
 import com.miniprojet.location_des_voitures.service.IVoitureService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/voitures")
@@ -18,36 +23,86 @@ public class VoitureController {
     @GetMapping
     //display all voitures
     public String getAllVoitures(Model model){
-        return null;
+        List<Voiture> voitures = voitureService.getAllVoitures();
+        model.addAttribute("voitures", voitures);
+        return "backoffice/voiture/index";
     }
 
     @GetMapping("/create")
     //display create voiture form
     public String createVoiture(Model model){
-        return null;
+        return "backoffice/voiture/create";
     }
 
     @PostMapping("/create")
     //create voiture
     public String createVoiture(Model model, @Valid  @RequestBody VoitureRequest voitureRequest, BindingResult bindingResult){
-        return null;
+        if (bindingResult.hasErrors()){
+            return "backoffice/voiture/create";
+        }
+        Voiture voiture = new Voiture(
+                voitureRequest.getImmatriculation(),
+                voitureRequest.getMarque(),
+                voitureRequest.getModele(),
+                voitureRequest.getDateDeMiseEnCirculation(),
+                voitureRequest.getPrixDeLocation(),
+                EStatut.Disponible,
+                voitureRequest.getTypeDeCarburant(),
+                voitureRequest.getKilometrage(),
+                voitureRequest.getTypeDeTransmission(),
+                voitureRequest.getCouleur(),
+                voitureRequest.getNombreDePlaces(),
+                voitureRequest.getOptions(),
+                voitureRequest.getImage(),
+                null
+        );
+        voitureService.createVoiture(voiture);
+        return "redirect:/voitures";
     }
 
     @GetMapping("/{id}/edit")
     //display edit voiture form
     public String editVoiture(Model model, @PathVariable Long id){
-        return null;
+        Optional<Voiture> voiture = voitureService.getVoitureById(id);
+        if (voiture.isPresent()){
+            model.addAttribute("voiture", voiture.get());
+            return "backoffice/voiture/edit";
+        }
+        return "redirect:/voitures";
     }
 
     @PostMapping("/{id}/edit")
     //edit voiture
     public String editVoiture(Model model, @PathVariable Long id, @Valid @RequestBody VoitureRequest voitureRequest, BindingResult bindingResult){
-        return null;
+        if (bindingResult.hasErrors()){
+            return "backoffice/voiture/edit";
+        }
+        Optional<Voiture> voiture = voitureService.getVoitureById(id);
+        if (voiture.isPresent()){
+            voiture.get().setImmatriculation(voitureRequest.getImmatriculation());
+            voiture.get().setMarque(voitureRequest.getMarque());
+            voiture.get().setModele(voitureRequest.getModele());
+            voiture.get().setDateDeMiseEnCirculation(voitureRequest.getDateDeMiseEnCirculation());
+            voiture.get().setPrixDeLocation(voitureRequest.getPrixDeLocation());
+            voiture.get().setTypeDeCarburant(voitureRequest.getTypeDeCarburant());
+            voiture.get().setKilometrage(voitureRequest.getKilometrage());
+            voiture.get().setTypeDeTransmission(voitureRequest.getTypeDeTransmission());
+            voiture.get().setCouleur(voitureRequest.getCouleur());
+            voiture.get().setNombreDeplaces(voitureRequest.getNombreDePlaces());
+            voiture.get().setOptions(voitureRequest.getOptions());
+            voiture.get().setImage(voitureRequest.getImage());
+            voitureService.updateVoiture(voiture.get());
+        }
+        return "redirect:/voitures";
     }
 
     @PostMapping("/{id}/delete")
     //delete voiture
     public String deleteVoiture(Model model, @PathVariable Long id){
-        return null;
+        Optional<Voiture> voiture = voitureService.getVoitureById(id);
+        if (voiture.isPresent()){
+            voitureService.deleteVoiture(id);
+        }
+        return "redirect:/voitures";
     }
 }
--- src/main/java/com/miniprojet/location_des_voitures/dto/requests/VoitureRequest.java
+++ src/main/java/com/miniprojet/location_des_voitures/dto/requests/VoitureRequest.java
@@ -16,7 +16,7 @@ import java.util.Date;
 @Setter
 public class VoitureRequest {
     @NotNull(message = "L'immatriculation est requise")
-    private Long immatriculation;
+    private String immatriculation;
     @NotBlank(message = "La marque est requise")
     private String marque;
     @NotBlank(message = "Le modèle est requis")
@@ -26,8 +26,6 @@ public class VoitureRequest {
     @NotNull(message = "Le prix de location est requis")
     @Min(value = 0, message = "Le prix de location doit être supérieur ou égal à 0")
     private Double prixDeLocation;
-    @NotNull(message = "Le statut de disponibilité est requis")
-    private EStatut statutDeDisponibilite;
     @NotNull(message = "Le type de carburant est requis")
     private ECarburant typeDeCarburant;
     @NotNull(message = "Le kilométrage est requis")
@@ -40,4 +38,8 @@ public class VoitureRequest {
     @NotNull(message = "Le nombre de places est requis")
     @Min(value = 1, message = "Le nombre de places doit être d'au moins 1")
     private Short nombreDePlaces;
+    @NotBlank(message = "Les options sont requises")
+    private String options;
+    @NotBlank(message = "L'image est requise")
+    private String image;
 }
