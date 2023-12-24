5131599acadeee121f8cfb051beefb11404ebae4
Afli Mohamed Ghayth <40703798+ghayth-afli@users.noreply.github.com>
2023-12-24 13:09:05 +0100

    add tableau de bord vue

--- src/main/java/com/miniprojet/location_des_voitures/controller/TableauDeBordController.java
+++ src/main/java/com/miniprojet/location_des_voitures/controller/TableauDeBordController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/tableau-de-bord")
